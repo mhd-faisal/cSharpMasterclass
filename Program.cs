8818b6ec773b9dcd728816c68c78f51bc0ab5b43
mhd-faisal <120516356+mhd-faisal@users.noreply.github.com>
2023-07-11 14:45:30 +1200

    uploadingFiles
    Uploading initial set of files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Console.WriteLine("Hello!");
+//Console.WriteLine("[S]ee all TODOs");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
+
+
+//Console.ReadKey();
+
+int a = 10;
+int b = 5;
+
+Console.WriteLine(a + b);   
+Console.WriteLine(a - b);   
+Console.WriteLine(a * b);   
+Console.WriteLine(a / b);
+
+Console.ReadKey();
