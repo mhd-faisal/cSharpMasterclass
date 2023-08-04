7ddc512d29d70bd753f0cda5e5562ef238fbefa7
mhd-faisal <mhd.faisal1990@gmail.com>
2023-08-04 13:54:38 +1200

    Updating files

---
+++ Utilities/EnumberableExtentions.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public static class EnumberableExtentions
+{
+    public static int SumOfEvenNumbers(this IEnumerable<int> numbers)
+    {
+        int sum = 0;
+        foreach (var number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                sum += number;
+            }
+        }    
+        return sum;
+    }
+}
